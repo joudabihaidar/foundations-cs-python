ed3dd00f657d272815cff8a0c25a1679d33857da
Joud <joudabihaidar2@gmail.com>
2023-07-13 00:04:22 +0300

    assignment 2 v2

--- assignment_02_Joud_AbiHaidar.py
+++ assignment_02_Joud_AbiHaidar.py
@@ -7,6 +7,7 @@ Created on Wed Jul 12 23:50:40 2023
 ############
 # Choice 1 #
 ############
+
 def count(n):
     if n==0 or n==-1:
         return 0
@@ -15,3 +16,22 @@ def count(n):
 n=int(input("enter a negative or positive number:"))
 print(count(n))
 
+############
+# Choice 2 #
+############
+
+def maximum(l):
+    if len(l)==1:
+        return l[0]
+    elif len(l)==0:
+        return 0
+    elif l[0]>l[1]:
+        #return maximum(l[0:1]+l[2::])
+        del l[1]
+        return maximum(l)
+    else:
+        del l[0]
+        return maximum(l)
+        #return maximum(l[1::])
+l=[-1,-3,-5,-4,10,90,80]
+print(maximum(l))
