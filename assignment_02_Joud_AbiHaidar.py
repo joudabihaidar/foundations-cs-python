43d9c376a10bed9adc0a802caf2693f223d6ecb2
Joud <joudabihaidar2@gmail.com>
2023-07-14 14:07:53 +0300

    adding choice 3 in menu

--- assignment_02_Joud_AbiHaidar.py
+++ assignment_02_Joud_AbiHaidar.py
@@ -37,7 +37,7 @@ def maximum(l):
 ########
 
 def main():
-    choices=['1','2','3','4']
+    choices=('1','2','3','4') #tuple since we only have these choices
     print("1. Count Digits\n2. Find Max\n3. Count tags\n4. Exit")
     print("- - - - - - - - - - - - - - -")
     choice=input("Enter a choice:")
@@ -56,7 +56,31 @@ def main():
                     value=int(input(f"enter value {i}:"))
                     l.append(value)
                 print(maximum(l))
+            elif choice=='3':
+                ######################################################################################          
+                # This list includes essential tags for defining the structure, headings, paragraphs,#
+                # links, images, lists, tables, forms, and basic formatting elements in HTML.        #
+                ######################################################################################
+                
+                html_tags = ["html", "body", "div", "p", "a", "img", "ul", "ol", "li", "h1", "h2", "h3", "h4",
+                             "h5", "h6", "head", "title", "meta", "link", "script", "style", "table", "tr", "td",
+                             "th", "tbody", "tfoot", "thead", "form", "input", "label", "button",
+                             "select", "option", "textarea", "span", "br", "hr"]
+                tag=input("Enter the tag that you want:")
+                while tag not in html_tags:
+                    tag=input("Enter the tag that you want:")
+                #html_file=input("enter the path of your html file:")
+                html_file='C:/Users/Legion/Desktop/quizHtml/cv.html'
+                with open(html_file, 'r') as file:
+                    html_content=file.read()
+                print(html_content)
+                
             choice=input("Enter a choice:")
             while (not choice.isnumeric()) or choice not in choices:
                 choice=input("Enter a choice:") 
+                
+                
+
+
+
 main()
